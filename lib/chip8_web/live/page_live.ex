109ed989892a12a6a2f7caba69d1054b6c23e92c
Quinn Wilton <wilton@synopsys.com>
2020-08-22 20:40:34 -0700

    wip

--- lib/chip8_web/live/page_live.ex
+++ lib/chip8_web/live/page_live.ex
@@ -22,7 +22,41 @@ defmodule Chip8Web.PageLive do
         roms: ROMs.list_roms(),
         rom: nil,
         last_cycle: nil,
-        running: false
+        running: false,
+        instructions: [],
+        stack_addresses: [],
+        row0: [],
+        row1: [],
+        row2: [],
+        row3: [],
+        row4: [],
+        row5: [],
+        row6: [],
+        row7: [],
+        row8: [],
+        row9: [],
+        row10: [],
+        row11: [],
+        row12: [],
+        row13: [],
+        row14: [],
+        row15: [],
+        row16: [],
+        row17: [],
+        row18: [],
+        row19: [],
+        row20: [],
+        row21: [],
+        row22: [],
+        row23: [],
+        row24: [],
+        row25: [],
+        row26: [],
+        row27: [],
+        row28: [],
+        row29: [],
+        row30: [],
+        row31: []
       )
 
     {:ok, socket}
@@ -132,10 +166,81 @@ defmodule Chip8Web.PageLive do
         Emulator.step(acc)
       end)
 
+    screen = :chip8@screen.to_list(emulator.screen)
+
+    [
+      row0,
+      row1,
+      row2,
+      row3,
+      row4,
+      row5,
+      row6,
+      row7,
+      row8,
+      row9,
+      row10,
+      row11,
+      row12,
+      row13,
+      row14,
+      row15,
+      row16,
+      row17,
+      row18,
+      row19,
+      row20,
+      row21,
+      row22,
+      row23,
+      row24,
+      row25,
+      row26,
+      row27,
+      row28,
+      row29,
+      row30,
+      row31
+    ] = Enum.chunk_every(screen, 64)
+
     socket =
       socket
       |> assign(:emulator, emulator)
       |> assign(:last_cycle, now)
+      |> assign(:instructions, disassemble_instructions(emulator))
+      |> assign(:stack_addresses, list_stack_addresses(emulator))
+      |> assign(:row0, row0)
+      |> assign(:row1, row1)
+      |> assign(:row2, row2)
+      |> assign(:row3, row3)
+      |> assign(:row4, row4)
+      |> assign(:row5, row5)
+      |> assign(:row6, row6)
+      |> assign(:row7, row7)
+      |> assign(:row8, row8)
+      |> assign(:row9, row9)
+      |> assign(:row10, row10)
+      |> assign(:row11, row11)
+      |> assign(:row12, row12)
+      |> assign(:row13, row13)
+      |> assign(:row14, row14)
+      |> assign(:row15, row15)
+      |> assign(:row16, row16)
+      |> assign(:row17, row17)
+      |> assign(:row18, row18)
+      |> assign(:row19, row19)
+      |> assign(:row20, row20)
+      |> assign(:row21, row21)
+      |> assign(:row22, row22)
+      |> assign(:row23, row23)
+      |> assign(:row24, row24)
+      |> assign(:row25, row25)
+      |> assign(:row26, row26)
+      |> assign(:row27, row27)
+      |> assign(:row28, row28)
+      |> assign(:row29, row29)
+      |> assign(:row30, row30)
+      |> assign(:row31, row31)
 
     {:noreply, socket}
   end
